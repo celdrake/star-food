241a1e2931b0153c4f02a671417393bdc8ed3dbf
Celia Amador <celdrake@gmail.com>
2015-10-26 23:08:09 +0000

    Pagination fully working. Improve colors

--- server/recipes.js
+++ server/recipes.js
@@ -9,18 +9,21 @@ function findAllRecipes(req, res) {
     db.findAllRecipes(function (err, recipes) {
         if (err === null && recipes.length > 0) {
             var recipesNumber = recipes.length;
-            if (recipesNumber === 1) {
-                // Return the full details of the recipe
-                res.send(recipes[0]);
-            } else if (recipesNumber < 10) {
+            if (recipesNumber < 10) {
                 // Return a single page with all the recipes
-                res.send(recipes);
+                res.send({
+                    totalResults: recipes.length,
+                    recipes: recipes
+                });
             } else {
                 // Paginate response
                 var page = parseInt(req.query.page || 0, 10);
                 var pageStart = page * RECIPES_PER_PAGE;
                 var pageEnd = pageStart + RECIPES_PER_PAGE;
-                res.send(recipes.slice(pageStart, pageEnd));
+                res.send({
+                    totalResults: recipes.length,
+                    recipes: recipes.slice(pageStart, pageEnd)
+                });
             }
         } else {
             // Error while retrieving the recipes, or none exists
--- src/js/recipes.js
+++ src/js/recipes.js
@@ -2,35 +2,76 @@
 
 
 var SERVER_URI = 'http://localhost:8080/';
+var RECIPES_PER_PAGE = 10;
 
+var currentPage;
+var maxPages;
 
 function initialiseRecipeList() {
-    fillAllRecipes();
+    currentPage = 0;
+    loadBBCRecipes();
 }
 
-function fillAllRecipes() {
+function loadBBCRecipes() {
     var recipesResponse = '';
-    jQuery.get(SERVER_URI + 'recipes', function onSuccess(recipes) {
-        if (recipes.length === 1) {
-            // TODO check
-            window.location.href = 'recipe.html?id=' + recipes[0].id;
-            return;
-        }
-        var recipeElems = [];
-        recipes.forEach(function (recipe) {
-            var attrId = 'recipe-' + recipe.id;
-            var a = '<a class="recipe__link" href="recipe.html?id=' + recipe.id + '">' + 'View Details ➤' + '</a>';
-            var name = '<span class="recipe__name">' + recipe.name + '</span>';
-            var elem = jQuery('<div class="recipe" data-id="' + attrId + '"></div>')
-                .append(name).append(a);
-            recipeElems.push(elem);
-        });
-        recipesResponse = recipeElems;
+    jQuery.get(SERVER_URI + 'recipes?page=' + currentPage, function onSuccess(response) {
+        recipesResponse = generateRecipeList(response);
     }).fail(function(err) {
         recipesResponse = err.responseText;
     }).always(function () {
         populateRecipeList(recipesResponse);
     });
+}
+
+function generateRecipeList(response) {
+    var totalRecipes = response.totalResults;
+    if (totalRecipes === 1) {
+        // TODO should list the recipe in the current page!!!
+        window.location.href = 'recipe.html?id=' + recipes[0].id;
+        return;
+    }
+    var recipeElems = [];
+    response.recipes.forEach(function (recipe, idx) {
+        var attrId = 'recipe-' + recipe.id;
+        var a = '<a class="recipe__link" href="recipe.html?id=' + recipe.id + '">' + 'View Details ➤' + '</a>';
+        var name = '<span class="recipe__name">' + recipe.name + '</span>';
+        var clazz = 'recipe';
+        if (idx % 2 === 0) {
+            clazz += ' recipe--odd';
+        }
+        var elem = jQuery('<div class="' + clazz + '" data-id="' + attrId + '"></div>')
+            .append(name).append(a);
+        recipeElems.push(elem);
+    });
+    drawPagination(totalRecipes);
+    return recipeElems;
+}
+
+function nextPage() {
+    // Ensure limits
+    if (currentPage < maxPages - 1) {
+        currentPage++;
+        loadBBCRecipes();
+    }
+}
+
+function previousPage() {
+    // Ensure limits
+    if (currentPage > 0) {
+        currentPage--;
+        loadBBCRecipes();
+    }
+}
+
+function drawPagination(totalRecipes) {
+    maxPages = Math.ceil(totalRecipes / RECIPES_PER_PAGE);
+    var paginationArrows = jQuery('#recipes-pagination').find('.pagination__arrow');
+    // Disable Next/Previous arrows when current page is in either of the limits (or both if only 1 page)
+    jQuery(paginationArrows[0]).toggleClass('pagination__arrow--disabled', currentPage === 0);
+    jQuery(paginationArrows[1]).toggleClass('pagination__arrow--disabled', currentPage === maxPages - 1);
+
+    // Update total pages
+    jQuery('#pagination-page').html('Page ' + (currentPage + 1) + ' / ' + maxPages);
 }
 
 function populateRecipeList(data) {
